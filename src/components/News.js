d7a1eaae5a098c245c77948d481932b1642d006b
vinnu9112 <111413210+vinnu9112@users.noreply.github.com>
2022-11-23 20:56:23 +0530

    eod1 commit

--- src/components/News.js
+++ src/components/News.js
@@ -9,16 +9,48 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1
     };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2123eecdcde14cabae76ee3b50698dc9";
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2123eecdcde14cabae76ee3b50698dc9&page=1&pagesize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+  }
+
+  handlePrevClick =  async ()=>{
+    console.log("prev");
+    let url =
+    `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2123eecdcde14cabae76ee3b50698dc9&page=${this.state.page - 1}&pagesize=20`;
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  console.log(parsedData);
+  this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+  })
+  }
+  handleNextClick =  async ()=>{
+    console.log("next");
+    if(this.state.page+1 >Math.ceil(this.state.totalResults/20)){
+
+    }
+
+    else{
+    let url =
+    `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2123eecdcde14cabae76ee3b50698dc9&page=${this.state.page + 1}&pagesize=20`;
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  console.log(parsedData);
+  this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+  })
+    }
   }
   render() {
     return (
@@ -36,6 +68,11 @@ export class News extends Component {
             />
           </div>  
         })} {/*md-4 --> 4 cols i medium devices */}
+        </div>
+        
+        <div className="container d-flex justify-content-between">
+        <button disabled= {this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous </button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,14 +9,14 @@ export class NewsItem extends Component {
         <div>
           <div className="card my-3" style={{ width: "18rem" }}>
             <img
-              src={imageUrl}
+              src={!imageUrl?"https://images.livemint.com/img/2022/11/23/600x338/multibagger_stock_1669182937458_1669182937667_1669182937667.jpg":imageUrl}
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{desc}</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+              <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark btn-primary">
                 Read More
               </a>
             </div>
