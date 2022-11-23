5c03815b2e6812fe0717f78f10786c55ecaaaf09
vinnu9112 <111413210+vinnu9112@users.noreply.github.com>
2022-11-23 20:14:34 +0530

    second commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -14,8 +14,8 @@ export class NewsItem extends Component {
               alt="..."
             />
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{desc}...</p>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{desc}</p>
               <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
                 Read More
               </a>
