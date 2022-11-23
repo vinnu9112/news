718cc14c6e8de3697d3ff682f877f2c699afe247
vinnu9112 <111413210+vinnu9112@users.noreply.github.com>
2022-11-23 19:39:26 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+    
+  render() {
+    let { title, desc, imageUrl, newsUrl } = this.props;
+    return (
+      <>
+        <div>
+          <div className="card my-3" style={{ width: "18rem" }}>
+            <img
+              src={imageUrl}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">{title}...</h5>
+              <p className="card-text">{desc}...</p>
+              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default NewsItem;
