ab832a0d39d9b6af412eb5d69df995865da5627c
vinnu9112 <111413210+vinnu9112@users.noreply.github.com>
2022-12-01 12:33:19 +0530

    minor change

--- src/components/News.js
+++ src/components/News.js
@@ -31,6 +31,7 @@ const News =(props)=> {
   useEffect(() => {
     document.title = `${capitalize(props.category)} - NewsMonkey`;
     updateNews();
+    // eslint-disable-next-line 
   }, []);
   
   const fetchMoreData = async() => {
